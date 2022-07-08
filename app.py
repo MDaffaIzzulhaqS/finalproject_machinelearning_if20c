21754199ea81e153fe11f1a29741242e4c66b996
MDaffaIzzulhaqS <izzulhaqdaffasuyanto@gmail.com>
2022-07-08 10:48:59 +0700

    Finishing Aplikasi

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import numpy as np
+import pickle
 
 app = Flask(__name__)
 
@@ -15,24 +16,23 @@ def about():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-   features = [float(x) for x in request.form.values()]
-   final_features = [np.array(features)]
-   prediction = model.predict(final_features)
-
-   output = round(prediction[0],2)
+    features = [float(x) for x in request.form.values()]
+    final_features = [np.array(features)]
+    prediction = model.predict(final_features)
+    
+    output = round(prediction[0],2)
 
     if output == 1:
-        out = 'Anda terkena Liver'
+        out = 'Harga Diprediksi Naik'
     else:
-        out = 'Anda tidak terkena Liver'
+        out = 'Harga Diprediksi Turun'
         
-    return render_template('result_predict.html', prediction_text='{}'.format(out))
+    return render_template('predict.html', prediction_text='{}'.format(out))
 
-"""
-@app.route("/predict")
+@app.route("/predict_page")
 def predict():
-   return render_template('predict.html')
+    return render_template('predict.html')
 
 if __name__ == "__main__":
     app.run()
-"""
+
